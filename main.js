072459a6cb828885426235e32ab7670efe17b6a4
math20122004 <mrsilva20122004@gmail.com>
2024-06-11 18:09:15 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
-
-document.addEventListener('DOMContentLoaded', function () {
-        var audio = document.getElementById('audioPlayer');
-        audio.muted = false; // Permitindo o áudio
-});
-
