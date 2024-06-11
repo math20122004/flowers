dca7dc0f95eac9bc699651d7527192c9dc7fec1f
math20122004 <mrsilva20122004@gmail.com>
2024-06-11 18:04:43 -0300

    tentando ser automatico

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 onload = () =>{
         document.body.classList.remove("container");
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+        var audio = document.getElementById('audioPlayer');
+        audio.muted = false; // Permitindo o áudio
+});
+
